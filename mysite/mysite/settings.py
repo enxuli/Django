626e0583c42e5a80d93997989a7c3b418458fb9d
AlexEXL <enxuli@usc.edu>
2018-09-15 02:09:47 -0700

    Build app before the generic view comes up!

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['polls.templates.polls',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
