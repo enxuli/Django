0a039fcc95544b3bb6ea542065a33ea7991e701c
AlexEXL <enxuli@usc.edu>
2018-09-15 00:41:14 -0700

    App starts here

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,11 +1,25 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length = 200)
-    pub_data = models.DateTimeField('date published')
-    
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+"""
+Django's ORM follows the relationship backwards from Question too,
+automatically generating a field on each instance called foo_set
+where Foo is the model with a ForeignKey field to that model.
+"""
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
