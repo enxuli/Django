25a49b809b9f8d4e98f4ebda48c361c8ecd15401
AlexEXL <enxuli@usc.edu>
2018-09-15 19:36:47 -0700

    Add test for all models and add style sheet

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 """
 Django's ORM follows the relationship backwards from Question too,
 automatically generating a field on each instance called foo_set
